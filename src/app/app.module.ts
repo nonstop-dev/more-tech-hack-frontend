c8d32204c815af81362e173a17b084deacb9e361
asurgutov <surgutov55@gmail.com>
2023-10-14 02:49:40 +0300

    added ya-maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SearchCardComponent } from './components/search-card/search-card.component';
 import { PointCardComponent } from './components/point-card/point-card.component';
+import { AngularYandexMapsModule, YaConfig } from 'angular8-yandex-maps';
+
+const mapConfig: YaConfig = {
+  apikey: '9899ecd3-56df-43c6-bf59-54fd17eb8b6b',
+  lang: 'ru_RU',
+};
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, SearchCardComponent, PointCardComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, AngularYandexMapsModule.forRoot(mapConfig)],
   providers: [],
   bootstrap: [AppComponent],
 })
