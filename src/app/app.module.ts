7a1f0fcfcbcf3b2ffb55acafc2c2a7ae57bda74a
asurgutov <surgutov55@gmail.com>
2023-10-14 01:45:47 +0300

    added initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SearchCardComponent } from './components/search-card/search-card.component';
+import { PointCardComponent } from './components/point-card/point-card.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, SearchCardComponent, PointCardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/point-card/point-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-point-card',
+  templateUrl: './point-card.component.html',
+  styleUrls: ['./point-card.component.scss']
+})
+export class PointCardComponent {
+
+}
