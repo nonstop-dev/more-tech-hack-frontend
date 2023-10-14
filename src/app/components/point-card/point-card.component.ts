4770cef0df96a51c07707794e5110b6fdf4cd28f
asurgutov <surgutov55@gmail.com>
2023-10-15 01:34:11 +0300

    added initial second card

--- src/app/components/point-card/point-card.component.ts
+++ src/app/components/point-card/point-card.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-point-card',
   templateUrl: './point-card.component.html',
-  styleUrls: ['./point-card.component.scss']
+  styleUrls: ['./point-card.component.scss'],
 })
-export class PointCardComponent {
-
-}
+export class PointCardComponent {}
